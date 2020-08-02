1dbb56587d4cb5758ac1918f91817a19107d8996
Theresa Medina <trgmedina@gmail.com>
2020-08-01 21:43:33 -0500

    initial setup

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php /* 
+This page is used to display the static frontpage. 
+*/
+ 
+// Fetch theme header template
+get_header(); ?>
+    <div id="primary">
+ 
+    <div class="custom-homepage-container"> 
+ 
+    You custom homepage code goes here
+ 
+    </div> 
+ 
+    </div>
+    
+<?php get_footer(); ?>
