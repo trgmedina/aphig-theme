bb80f42aad307a9bc6d168580edf277d907ed73e
Theresa Medina <trgmedina@gmail.com>
2020-08-01 22:25:01 -0500

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+})
+
